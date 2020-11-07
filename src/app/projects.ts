9ab74c88e506ce80a413e4a27b3cbba5033f5ac8
DavidPhantom <davidnin@mail.ru>
2020-11-08 01:51:58 +0300

    Add new data to tables

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -4,5 +4,8 @@ import {TODOS_DATA} from "./todos";
 export const PROJECTS_DATA: Project[] = [
   {id: 1, title: 'Семья', todos: TODOS_DATA[0]},
   {id: 2, title: 'Работа', todos: TODOS_DATA[1]},
-  {id: 3, title: 'Прочее', todos: TODOS_DATA[2]},
+  {id: 3, title: 'Отдых', todos: TODOS_DATA[2]},
+  {id: 4, title: 'Спорт', todos: TODOS_DATA[3]},
+  {id: 5, title: 'Хобби', todos: TODOS_DATA[4]},
+  {id: 6, title: 'Прочее', todos: TODOS_DATA[5]}
 ];
